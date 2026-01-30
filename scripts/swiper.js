9d1366efdc6d2db61946120d96ac07fa7ff0e36e
samlmcewan <samlmcewan@gmail.com>
2026-01-30 22:05:11 +0000

    top bar footer

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -22,6 +22,18 @@ const swiper2 = new Swiper('.swiper2', {
   spaceBetween: 5
 });
 
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 4000, 
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
+  spaceBetween: 5
+});
+
 
 
 
